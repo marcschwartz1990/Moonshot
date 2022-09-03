e0ae16a6e39efc282267aa7e8e7b86485a49d277
Marc Schwartz <marcschwartz1990@gmail.com>
2022-09-02 20:43:50 -0400

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Marc-Developer on 9/2/22.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
